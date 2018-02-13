9f05eb61067d5b0d9c31beaca85d6ee3247609d3
Nexao <emilklausen2@hotmail.com>
2018-02-13 11:56:31 +0100

    Updated some of the methods and changed the prints

--- 02/twitter_queries.py
+++ 02/twitter_queries.py
@@ -9,8 +9,16 @@ def get_total_user_amount():
     print (len(db.tweets.distinct('user')))
 
 
-def get_users_who_mentions_others():
-    print ('???')
+def get_most_linking_users():
+    pipeline = [
+        {'$match':{'text':{'$regex':"@\w+"}}},
+        {'$addFields': {"mentions":1}},
+        {'$group':{"_id":"$user", "mentions":{'$sum':1}}},
+        {'$sort':{"mentions":-1}},
+        {'$limit':10}]
+    cursorlist = db.tweets.aggregate(pipeline)
+    for cursor in cursorlist:
+        print(cursor)
 
 
 def get_most_mentioned_users():
@@ -26,45 +34,56 @@ def get_most_mentioned_users():
     print (list(tweets))
 
 
-def get_most_active_users():
+def get_most_active_user():
     pipeline = [
         {'$group': {'_id': "$user", 'total': {'$sum': 1}}},
         {'$sort': {'total': -1}},
         {'$limit': 10},
     ]
     users = db.tweets.aggregate(pipeline)
-    print(list(users))
+    for user in users:
+        print(user)
 
 
 def get_most_negative_tweets():
+    
     pipeline = [
-        {'$match': {'text': {'$regex': "worst$"}}},
-        {'$project': {'_id': 0, 'user': 1}},
-        {'$limit': 5},
+        {'$match':{'text': {'$regex':'pissed|mad|angry|sad|furious|outraged','$options':'g'}}},
+        {'$group':{'_id':"$user", 'emotion': {'$avg':"$polarity"}, 'total_negative_tweets': {'$sum': 1}}},
+        {'$sort':{ 'emotion': 1, 'total_negative_tweets':-1}},
+        {'$limit':5}
     ]
-    users = db.tweets.aggregate(pipeline)
-    print (list(users))
+    negativeUser = db.tweets.aggregate(pipeline)
+    for negUser in negativeUser:
+        print(negUser)
 
 
 def get_most_positive_tweets():
     pipeline = [
-        {'$match': {'text': {'$regex': "love$"}}},
-        {'$project': {'_id': 0, 'user': 1}},
-        {'$limit': 5},
+        {'$match':{'text': {'$regex':'happy|excited|great|amazing|love|enticed','$options':'g'}}},
+        {'$group':{'_id':"$user", 'emotion': {'$avg':"$polarity"},'total_positive_tweets': {'$sum': 1}}},
+        {'$sort':{ 'emotion': -1, 'total_positive_tweets':-1}},
+        {'$limit':5}
     ]
-    users = db.tweets.aggregate(pipeline)
-    print (list(users))
+    positiveUser = db.tweets.aggregate(pipeline)
+    for posUser in positiveUser:
+        print(posUser)
 
 
-print ('How many Twitter users are in the database?')
+print('How many Twitter users are in the database?')
 get_total_user_amount()
+print("------")
 print('Top 10 users link the most to other Twitter users?')
-get_users_who_mentions_others()
+get_most_linking_users()
+print("------")
 print('Top 5 most mentioned Twitter users? ')
 get_most_mentioned_users()
+print("------")
 print('Top 10 most active Twitter users are: ')
-get_most_active_users()
-print('Top 5 users tweeting word "worst": ')
+get_most_active_user()
+print("------")
+print('Top 5 users tweeting word "pissed|mad|angry|sad|furious|outraged": ')
 get_most_negative_tweets()
-print ('Top 5 users tweeting word "love": ')
+print("------")
+print ('Top 5 users tweeting word "happy|excited|great|amazing|love|enticed": ')
 get_most_positive_tweets()
